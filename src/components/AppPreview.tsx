90407b7a74d087e1f7a54cda6326fa024775f749
Moon920830 <lucasdavis0830@gmail.com>
2023-06-12 16:51:57 -0500

    Fixed app preview in large screens

--- src/components/AppPreview.tsx
+++ src/components/AppPreview.tsx
@@ -4,9 +4,9 @@ type Props = {};
 
 const AppPreview = (props: Props) => {
   return (
-    <div className="flex flex-col items-center justify-center">
+    <div className="flex flex-col items-center justify-center max-h-80vh">
         <h1 className="text-secondary text-center"> Envision Your Garden</h1>
-      <div className="carousel w-4/5">
+      <div className="carousel w-4/5 md:w-2/3 lg:w-1/2">
   <div id="slide1" className="carousel-item relative w-full">
     <img src="/homegif.gif" className="w-full" />
     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">

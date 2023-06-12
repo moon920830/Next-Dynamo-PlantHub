e82b2cc41499b8d48a1a9334ad6c5aabfa512634
Moon920830 <lucasdavis0830@gmail.com>
2023-06-12 02:05:32 -0500

    Updated account page to have various options, but only theme toggling is functional. Adjusted provider hook for theming to enable transition. Additionally, adjusted dynamodb to create a createdAt section when creating a user, allowing for landing page to have a user since stat. Need to now add styling to plant card and add plant form before deploying to production

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -1,39 +1,92 @@
 "use client";
-import { useContext } from "react";
-import { UserContext } from "../providers";
-import PlantCard from "../../components/PlantCard";
+import { useContext, useState } from "react";
+import { useSession } from "next-auth/react";
+import { UserContext, ThemeContext } from "../providers";
 import Loading from "../../components/Loading";
 import Credentials from "../../components/Credentials";
 export default function Home() {
-  const { data, loading, error } = useContext(UserContext);
+  const {data, loading, error } = useContext(UserContext);
+  const [checkedTab, setCheckedTab] = useState<String>("");
+  const {theme,toggleTheme} = useContext(ThemeContext)
+  const {status} = useSession()
   if (error === "Offline or Unauthenticated") {
-    return <Credentials/>;
+    return <Credentials />;
   } else if (error) {
     return <h1>Undiagnozed accound error</h1>;
   }
-  if(loading){
-    return (<><Loading/></>)
-  }
-  if (!data) {
+  if (loading) {
     return (
-      <div>
-        <section>Preview Stuff!</section>
-      </div>
+      <>
+        <Loading />
+      </>
     );
   }
+  if (status !=="authenticated") {
+    return <Credentials />;
+
+  }
+  const handleCheckBoxChange = (section: string) => {
+    if (checkedTab === section) {
+      setCheckedTab("");
+      return;
+    }
+    setCheckedTab(section);
+  };
 
   return (
-    <div className="flex flex-col h-full w-full">
-      <h1 className="text-center h-9">
-        Ready to try us out!?
-      </h1>
-      <div className="flex-1 bg-yellow-500 flex flex-col items-center justify-around ">
-        <div className="px-4 bg-white w-full md:w-9/10 lg:w-4/5 ">
-          <div className="grid grid-cols-2 md:grid-cols-3 gap-4 bg-black">
-            {data.plants.map((card, index) => (
-              <PlantCard key={index} card={card} />
-            ))}
-          </div>
+    <div className="flex flex-col h-full w-full items-center justify-around text-secondary max-w-lg mx-1 p-1">
+      <div className="collapse collapse-arrow bg-secondary text-primary mx-2">
+        <input
+          type="radio"
+          name="my-accordion-2"
+          checked={checkedTab === "Account" ? true : false}
+          onChange={() => handleCheckBoxChange("Account")}
+        />
+        <div className="collapse-title text-xl font-medium">
+          Account Settings
+        </div>
+        <div className="collapse-content text-primary">
+          <p>Account Details</p>
+          <p>Change Password</p>
+          <p>Sign Out Button</p>
+        </div>
+      </div>
+      <div className="collapse collapse-arrow bg-secondary text-primary">
+        <input
+          type="radio"
+          name="my-accordion-2"
+          checked={checkedTab == "Settings" ? true : false}
+          onChange={() => handleCheckBoxChange("Settings")}
+        />{" "}
+        <div className="collapse-title text-xl font-medium">
+          Application Settings
+        </div>
+        <div className="collapse-content text-primary">
+        <div className="form-control">
+  <label className="label cursor-pointer">
+    <span className="label-text text-primary">Light Mode</span> 
+    <input type="radio" name="radio-10" className="radio checked:bg-primary" onChange={() => toggleTheme("light")} checked={theme === "light"? true: false}/>
+  </label>
+</div>
+<div className="form-control">
+  <label className="label cursor-pointer">
+    <span className="label-text text-primary">Dark Mode</span> 
+    <input type="radio" name="radio-10" className="radio checked:bg-primary" onChange={() => toggleTheme("dark")}
+    checked={theme === "dark"? true: false}/>
+  </label>
+</div>
+        </div>
+      </div>
+      <div className="collapse collapse-arrow bg-secondary text-primary">
+        <input
+          type="radio"
+          name="my-accordion-2"
+          checked={checkedTab == "Membership" ? true : false}
+          onChange={() => handleCheckBoxChange("Membership")}
+        />{" "}
+        <div className="collapse-title text-xl font-medium">Membership</div>
+        <div className="collapse-content text-primary">
+          <p>Details To Come</p>
         </div>
       </div>
     </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,27 +5,42 @@ import PlantCard from "../components/PlantCard";
 import Loading from "../components/Loading";
 export default function Home() {
   const { data, loading, error } = useContext(UserContext);
+  console.log(data)
   if (error === "Offline or Unauthenticated") {
     return <h1>Unauthenticated Screen Home, Sign Up Online</h1>;
   } else if (error) {
     return <h1>Undiagnozed home error</h1>;
   }
-  if(loading){
-    return (<><Loading/></>)
-  }
-  if (!data) {
+  if (loading) {
     return (
-      <div>
-        <section>Preview Stuff!</section>
-      </div>
+      <>
+        <Loading />
+      </>
     );
   }
 
+  const options: Intl.DateTimeFormatOptions = {
+    month: "long",
+    day: "numeric",
+    year: "numeric",
+  };
+  const dateToFormat = new Date();
+  const formattedDate = dateToFormat.toLocaleDateString("en-US", options);
   return (
-    <div className="flex flex-col h-full w-full">
-      <h1 className="text-center h-9">
-        Welcome to your garden! {data.username}
-      </h1>
+    <div className="flex flex-col h-full w-full justify-around pt-4">
+      <div className="stats shadow bg-secondary flex justify-center">
+        <div className="stat lg:w-2/5 place-items-center">
+          <div className="stat-title text-primary">User</div>
+          <div className="stat-value text-primary">{data.username}</div>
+          <div className="stat-desc text-primary">Since Jan 1, 2023</div>
+        </div>
+
+        <div className="stat lg:w-2/5 place-items-center">
+          <div className="stat-title text-primary">Garden Size</div>
+          <div className="stat-value text-primary">4,200</div>
+          <div className="stat-desc text-primary">Manage your Garden</div>
+        </div>
+      </div>
       <div className="flex-1 bg-yellow-500 flex flex-col items-center justify-around ">
         <div className="px-4 bg-white w-full md:w-9/10 lg:w-4/5 ">
           <div className="grid grid-cols-2 md:grid-cols-3 gap-4 bg-black">
